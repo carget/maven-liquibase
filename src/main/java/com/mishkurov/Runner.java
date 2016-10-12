83552f3558da8055585803b7c32ec203aed02b9b
Anton Mishkurov <Anton_Mishkurov@epam.com>
2016-10-12 12:23:17 +0300

    Refactor: using try with resources and made methods smaller

--- src/main/java/com/mishkurov/Runner.java
+++ src/main/java/com/mishkurov/Runner.java
@@ -16,8 +16,7 @@ public class Runner {
     public static void main(String[] args) throws ClassNotFoundException {
         System.out.println("=====================Hello!=======================");
         Class.forName(JDBC_DRIVER);
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
-             Statement statement = connection.createStatement()) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS)) {
 
             printTableData(connection, "DATABASECHANGELOG");
             printTableData(connection, "PERSON");
@@ -28,30 +27,19 @@ public class Runner {
         System.out.println("=====================Bye!=======================");
     }
 
-    private static void printTableColumns(Connection connection, String tableNamePattern) throws SQLException {
-        DatabaseMetaData databaseMetaData = connection.getMetaData();
-
-        ResultSet result = databaseMetaData.getColumns(
-                null, null, tableNamePattern.toUpperCase(), null);
-
-        while (result.next()) {
-            String columnName = result.getString("COLUMN_NAME");
-            System.out.println("Column name:" + columnName);
-        }
-    }
-
     private static void printTableData(Connection connection, String tableName) throws SQLException {
-        //todo remove throws or change structure
         System.out.println("Table name:" + tableName);
         DatabaseMetaData databaseMetaData = connection.getMetaData();
-        ResultSet result = databaseMetaData.getColumns(null, null, tableName.toUpperCase(), null);
-        Statement statement = connection.createStatement();
-        //TODO remove sql injection
-        String sql = "SELECT * FROM " + tableName + ";";
-        ResultSet tableResultSet = statement.executeQuery(sql);
-//        while (result.next()) {
-//            System.out.println(String.format("Column %s: %s ", result.getString("COLUMN_NAME"),
-//                    tableResultSet.getString(result.getString("COLUMN_NAME"))));
-//        }
+        ResultSet columns = databaseMetaData.getColumns(null, null, tableName.toUpperCase(), null);
+        try (Statement statement = connection.createStatement()){
+            String sql = "SELECT * FROM " + tableName + ";";
+            ResultSet tables = statement.executeQuery(sql);
+            while (tables.next()) {
+                while (columns.next()) {
+                    System.out.println(String.format("Column %s: %s ", columns.getString("COLUMN_NAME"),
+                            tables.getString(columns.getString("COLUMN_NAME"))));
+                }
+            }
+        }
     }
 }
